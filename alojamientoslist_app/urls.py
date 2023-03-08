2bc4fd23eeea4c828712a32d33f8224e372505b9
damederos <damederos96@gmail.com>
2023-03-08 10:57:59 -0500

    mostrar los alojamiento por el id - froma sencilla

--- alojamientoslist_app/urls.py
+++ alojamientoslist_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from alojamientoslist_app.views import alojamiento_list
+from alojamientoslist_app.views import alojamiento_list, alojamiento_detail
 
 urlpatterns = [
-    path('list/', alojamiento_list, name='alojamiento-list'),
+    path('list/', alojamiento_list, name='alojamiento-listar'),
+    path('<int:indice>', alojamiento_detail, name='alojamiento-detalle')
 ]
--- alojamientoslist_app/views.py
+++ alojamientoslist_app/views.py
@@ -10,3 +10,14 @@ def alojamiento_list(request):
         'alojamientos': list(alojamientos.values())
     }
     return JsonResponse(data)
+
+def alojamiento_detail(request, indice):
+    alojamientos = Alojamiento.objects.get(pk=indice)
+    data = {
+        'nombre_alojamiento': alojamientos.nombre_alojamiento,
+        'capacidad': alojamientos.capacidad,
+        'activo': alojamientos.activo
+    }
+    return JsonResponse(data)
+
+    
