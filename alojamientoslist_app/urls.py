a8aa5c4dd977f36a464698be93ef6be0f6d4c1e8
damederos <damederos96@gmail.com>
2023-03-08 02:37:19 -0500

    corrigiendo errores

--- alojamientoslist_app/urls.py
+++ alojamientoslist_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from alojamientoslist_app.views import alojamiento_list
 
 urlpatterns = [
-    path('list/', alojamiento_list, name='alojamiento-list')
+    path('list/', alojamiento_list, name='alojamiento-list'),
 ]
