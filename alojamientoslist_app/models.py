0032945ea55120732b9d7d98457429f7fef8ca9e
damederos <damederos96@gmail.com>
2023-03-08 01:51:42 -0500

    Cree susper usuario y registre la appp en el proyecto

--- alojamientoslist_app/models.py
+++ alojamientoslist_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Alojamiento(models.Model):
+    nombre_alojamiento = models.CharField(max_length=100)
+    capacidad = models.IntegerField()
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre_alojamiento
