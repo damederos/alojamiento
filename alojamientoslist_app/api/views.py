909850d1a5a1dee5e07c9bade406aa049669ce0c
damederos <damederos96@gmail.com>
2023-03-08 21:51:14 -0500

    Funciones de listar con rest_framework y utilizando el api_view

--- alojamientoslist_app/api/views.py
+++ alojamientoslist_app/api/views.py
@@ -1,3 +1,16 @@
 from alojamientoslist_app.models import Alojamiento
+from alojamientoslist_app.api.serializers import AlojamientoSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view()
 def alojamiento_list(request):
+    alojamientos = Alojamiento.objects.all()
+    serializer = AlojamientoSerializer(alojamientos, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def alojamiento_detail(request, indice):
+    alojamientos = Alojamiento.objects.get(pk=indice)
+    serializer = AlojamientoSerializer(alojamientos)
+    return Response(serializer.data)
