b0a238d2d74551a6f92c82e28b94cfaa2a431014
Dayron <damederos96@gmail.com>
2025-11-14 04:52:40 -0500

    cambiar de nombre el proyecto para hacerlo repositorio publico

--- configuraciones/asgi.py
+++ configuraciones/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Tesis_Daniel project.
+ASGI config for configuraciones project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tesis_Daniel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuraciones.settings')
 
 application = get_asgi_application()
--- configuraciones/wsgi.py
+++ configuraciones/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Tesis_Daniel project.
+WSGI config for configuraciones project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tesis_Daniel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuraciones.settings')
 
 application = get_wsgi_application()
